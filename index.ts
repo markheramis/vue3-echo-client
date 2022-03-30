23d3f5ef93f07db20f11b3506f2163a88c226f2f
carlomar-espinosa <carlomarespinosa@gmail.com>
2022-03-31 05:13:24 +0800

    fixed condition

--- index.ts
+++ index.ts
@@ -54,7 +54,7 @@ export const echo = (options: Options) => {
 }
 
 export const broadcast = () => {
-    if (app.config.globalProperties.$echo != 'undefined') {
+    if (app.config.globalProperties.$echo === undefined) {
       return echo(app.config.globalProperties.$echoOptions)
     }
     return app.config.globalProperties.$echo;
